f18d495f3c1763e98cb6ddcbe6969ef7f1820bd1
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-05-15 21:36:57 +0700

    fix: fix views  and urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from api import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('api/', views.destination_list),
     # path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -2,28 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from inventory.model import *
 # Create your views here.
 
 
-@csrf_exempt
-def destination_list(request):
+@api_view(['GET', 'POST'])
+def destination_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
-    data = {'ini': 1, 'itu': 2}
+    # data = {'ini': 1, 'itu': 2}
     if request.method == 'GET':
         # snippets = Snippet.objects.all()
         # serializer = SnippetSerializer(snippets, many=True)
+        test = get_popular()
 
-        return JsonResponse(data, safe=False)
+        return JsonResponse(test, safe=False)
 
     elif request.method == 'POST':
         # data = JSONParser().parse(request)
         # serializer = SnippetSerializer(data=data)
         # if serializer.is_valid():
             # serializer.save()
-        
-        return JsonResponse(data, safe=False, status=201)
+        print(type(request.data['day']))
+        day = int(request.data['day'])
+        print(type(day))
+        test = get_popular(day)
+        return JsonResponse(test, safe=False, status=201)
         
         # return JsonResponse(serializer.errors, status=400)
