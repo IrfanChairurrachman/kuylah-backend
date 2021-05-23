e87cc667345e6071e1517879a8aa6f6664ae9db4
Dita Wiyoto <dswiyt@gmail.com>
2021-05-23 20:46:07 +0700

    change 'api/' to 'api'

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from api import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('api/', views.destination_list),
+    path('api', views.destination_list),
     # path('snippets/<int:pk>/', views.snippet_detail),
 ]
 
